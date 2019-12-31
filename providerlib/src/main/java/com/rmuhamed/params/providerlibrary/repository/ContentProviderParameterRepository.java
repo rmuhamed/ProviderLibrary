c6d89e378f671cca18bcf0b6ec3a5b206a67c16e
Ricardo Ángel Muhamed <ricardo.muhamed@gmail.com>
2019-12-31 11:37:13 +0100

    Queries should be done in a background thread so I handle it within Java concurrent work classes, i.e with the usage of: Futures, Callbacks & ExecutorService

--- providerlib/src/main/java/com/rmuhamed/params/providerlibrary/repository/ContentProviderParameterRepository.java
+++ providerlib/src/main/java/com/rmuhamed/params/providerlibrary/repository/ContentProviderParameterRepository.java
@@ -14,27 +14,35 @@ import com.rmuhamed.params.providerlibrary.QueryObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public final class ContentProviderParameterRepository implements ParameterRepository {
+    private final ExecutorService executorService;
     private Uri contentUri;
     private Context context;
 
-    public ContentProviderParameterRepository(Context context, Configuration configuration) {
+    public ContentProviderParameterRepository(Context context, ExecutorService executorService, Configuration configuration) {
         this.contentUri = configuration.getAuthorityUri();
         this.context = context;
+        this.executorService = executorService;
     }
 
     @Override
-    public boolean add(Param newParam) {
+    public Future<Boolean> add(Param newParam) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(ParamContract.NAME_PARAM_KEY, newParam.getName());
         contentValues.put(ParamContract.FORMAT_PARAM_KEY, newParam.getFormat());
 
-        return context.getContentResolver().insert(contentUri, contentValues) != null;
+        Callable<Boolean> call = () ->
+                context.getContentResolver().insert(contentUri, contentValues) != null;
+
+        return executorService.submit(call);
     }
 
     @Override
-    public boolean update(int paramId, Param updatedParam) {
+    public Future<Boolean> update(int paramId, Param updatedParam) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(ParamContract.NAME_PARAM_KEY, updatedParam.getName());
         contentValues.put(ParamContract.FORMAT_PARAM_KEY, updatedParam.getFormat());
@@ -43,49 +51,66 @@ public final class ContentProviderParameterRepository implements ParameterReposi
         String[] selectionArgs = new String[1];
         selectionArgs[0] = Integer.valueOf(paramId).toString();
 
-        return context.getContentResolver()
-                .update(contentUri, contentValues, selectionClause, selectionArgs) > 0;
+        Callable<Boolean> call = () ->
+                context.getContentResolver().update(contentUri, contentValues, selectionClause, selectionArgs) > 0;
+
+        return this.executorService.submit(call);
     }
 
     @Override
-    public List<Param> all() {
+    public Future<List<Param>> all() {
         return query(null);
     }
 
     @Override
-    public List<Param> query(@Nullable QueryObject queryObject) {
-        String selectionClause = null;
-        String[] selectionArgs = null;
+    public Future<List<Param>> query(@Nullable final QueryObject queryObject) {
+        Callable<List<Param>> call = () -> {
+            Cursor cursor = context.getContentResolver()
+                    .query(
+                            contentUri
+                            , null
+                            , getSelectionClause(queryObject)
+                            , getSelectionArgs(queryObject)
+                            , ""
+                    );
 
+            if (cursor == null) {
+                throw new RuntimeException("Could not be performed");
+            } else {
+                List<Param> result = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    int id = cursor.getInt(cursor.getColumnIndexOrThrow(ParamContract.ID_PARAM_KEY));
+                    String name = cursor.getString(cursor.getColumnIndexOrThrow(ParamContract.NAME_PARAM_KEY));
+                    String format = cursor.getString(cursor.getColumnIndexOrThrow(ParamContract.FORMAT_PARAM_KEY));
+
+                    result.add(ParamFactory.createFromDB(id, name, format));
+                }
+
+                cursor.close();
+                return result;
+            }
+        };
+
+        return this.executorService.submit(call);
+    }
+
+    private String[] getSelectionArgs(@Nullable final QueryObject queryObject) {
+        String[] selectionArgs = null;
         if (queryObject != null) {
-            selectionClause = String.format("%s=?", queryObject.getQueryName());
             selectionArgs = new String[1];
             selectionArgs[0] = queryObject.getQueryValue();
         }
 
-        Cursor cursor = context.getContentResolver()
-            .query(
-                contentUri
-                , null
-                , selectionClause
-                , selectionArgs
-                , ""
-            );
+        return selectionArgs;
+    }
 
-        if (cursor == null) {
-            throw new RuntimeException("Could not be performed");
-        } else {
-            List<Param> result = new ArrayList<>();
-            while (cursor.moveToNext()) {
-                int id = cursor.getInt(cursor.getColumnIndexOrThrow(ParamContract.ID_PARAM_KEY));
-                String name = cursor.getString(cursor.getColumnIndexOrThrow(ParamContract.NAME_PARAM_KEY));
-                String format = cursor.getString(cursor.getColumnIndexOrThrow(ParamContract.FORMAT_PARAM_KEY));
+    private String getSelectionClause(@Nullable final QueryObject queryObject) {
+        String selectionClause = null;
 
-                result.add(ParamFactory.createFromDB(id, name, format));
-            }
-
-            cursor.close();
-            return result;
+        if (queryObject != null) {
+            selectionClause = String.format("%s=?", queryObject.getQueryName());
         }
+
+        return selectionClause;
     }
 }
